19f003710ae40d91e3ff0b3fc2f356a14ca027f6
ubuntu <41911607+shyamtalekar@users.noreply.github.com>
2022-06-13 23:07:03 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+# variable "application" {
+#   type        = string
+#   description = "Name of the application"
+#   default = "ansible"
+# }
+
+# variable "environment" {
+#   type        = string
+#   description = "Name of the application environment"
+#   default = "dv"
+# }
+
+# variable "service" {
+#   type        = string
+#   description = "Name of the service"
+#   default = "development"
+# }
+
+# variable "vpc_group" {
+#   type = string
+#   description = "VPC Group Name"
+#   default = "policy-staging"
+# }
+
+# variable "vpc_tier" {
+#   type = string
+#   description = "VPC tier"
+#   default = "app"
+# }
